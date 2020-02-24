e27fd50bbbd0194319745c114a343670ee577bf4
Tro <noe.monsivais@gmail.com>
2020-02-24 02:29:12 -0600

    removed original alert and connected it to document.querySelector.  Starting again from scratch.  Attempt #2

--- develop/script.js
+++ develop/script.js
@@ -1,5 +1,23 @@
+//-----------------Add all variables here---------------------//
+var arrays = [
+  {
+    lowcase: ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+    upCase: ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+    num: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+    speChar: ["!", "#", "$", "%", "&", "*", ",", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "^", "_"] //special character length = 19//
+  }
+]
 // Assignment Code
-// var generateBtn = document.querySelector("#generate");
+var generateBtn = document.querySelector("#generate");
+generateBtn.onclick = function () {
+  alert("Hello boss.  Click ok to select your criteria.");
+  
+}
+
+
+
+
 
 // // Write password to the #password input
 // function writePassword() {
@@ -17,30 +35,24 @@
 
 //console log lowercase letters//
 
-//-----------------Add all variables here---------------------//
-var lowCase = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
-
-var upCase = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
-
-var num = [0,1,2,3,4,5,6,7,8,9]
-
-//special character length = 19//
-var speChar = ["!", "#", "$", "%", "&", "*", ",", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "^", "_",]
-
-// var characlength = prompt("Enter a number between '8' and '128'.");
 
 
 //--------------------Create Prompts here--------------------//
 
-function initClick() {
-
-  alert("Hello boss.  Click ok to select your criteria.");
   //All criteria will be prompted as below and stored in individual variables.//
-  var characlength = prompt("Enter a # between '8' and '128'");
-  var userlowercase = prompt("If you want lowercase letters type 'yes'.  Otherwise, type 'no'.");
-  var useruppercase = prompt("If you want uppercase letters type 'yes'.  Otherwise, type 'no'.");
-  var userspecialcharacters = prompt("If you want special characters type 'yes'.  Otherwise, type 'no'.");
 
-  console.log(characlength, userlowercase, useruppercase, userspecialcharacters);
-}
+  // var characlength = Number(prompt("Enter a # between '8' and '128'"));
+
+  // var userlowercase = prompt("If you want lowercase letters type 'yes'.  Otherwise, type 'no'.");
+
+  // var useruppercase = prompt("If you want uppercase letters type 'yes'.  Otherwise, type 'no'.");
+
+  // var userspecialcharacters = prompt("If you want special characters type 'yes'.  Otherwise, type 'no'.");
+//-----If Statements Below-----------//
+  // if (characlength < 8 || characlength > 128) {
+  //   alert("Number must be between 8 and 128");
+  // }
+
+  // console.log(characlength, userlowercase, useruppercase, userspecialcharacters);
+// }
 
