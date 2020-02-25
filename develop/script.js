9c51b40de47397231e62bbc0a8203af468a5618c
Tro <noe.monsivais@gmail.com>
2020-02-24 19:47:18 -0600

    Deleted most of the code and started fresh.  Included empty variables.

--- develop/script.js
+++ develop/script.js
@@ -1,58 +1,23 @@
-//-----------------Add all variables here---------------------//
-var arrays = [
-  {
-    lowcase: ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-    upCase: ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
-    num: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+// variables //
+var generateBtn = document.querySelector("#generate");
+var userLowCase;
+var userUpCase;
+var userSpecChar;
+var userNumChoice;
+var userPassLength;
 
-    speChar: ["!", "#", "$", "%", "&", "*", ",", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "^", "_"] //special character length = 19//
-  }
-]
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
-generateBtn.onclick = function () {
-  alert("Hello boss.  Click ok to select your criteria.");
-  
+
+// Write password to the #password input
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+  passwordText.value = password;
+
 }
 
-
-
-
-
-// // Write password to the #password input
-// function writePassword() {
-//   var password = generatePassword();
-//   var passwordText = document.querySelector("#password");
-
-//   passwordText.value = password;
-
-// }
-
-// // Add event listener to generate button
-// generateBtn.addEventListener("click", writePassword);
-
-//------------------------------------------------------//
-
-//console log lowercase letters//
-
-
-
-//--------------------Create Prompts here--------------------//
-
-  //All criteria will be prompted as below and stored in individual variables.//
-
-  // var characlength = Number(prompt("Enter a # between '8' and '128'"));
-
-  // var userlowercase = prompt("If you want lowercase letters type 'yes'.  Otherwise, type 'no'.");
-
-  // var useruppercase = prompt("If you want uppercase letters type 'yes'.  Otherwise, type 'no'.");
-
-  // var userspecialcharacters = prompt("If you want special characters type 'yes'.  Otherwise, type 'no'.");
-//-----If Statements Below-----------//
-  // if (characlength < 8 || characlength > 128) {
-  //   alert("Number must be between 8 and 128");
-  // }
-
-  // console.log(characlength, userlowercase, useruppercase, userspecialcharacters);
-// }
-
+// Add event listener to generate button
+generateBtn.addEventListener("click", () => {
+ writePassword;
+});
