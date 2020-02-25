5499b7097c308f881780a9486157fc4fa6adcdcb
Tro <noe.monsivais@gmail.com>
2020-02-24 21:34:48 -0600

    Added rest of confirm prompts.  Added arrays that include lower and upper case letters, special characters, and numbers.

--- develop/script.js
+++ develop/script.js
@@ -1,11 +1,11 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 // variables //
-var userLowCase;
-var userUpCase;
-var userSpecChar;
-var userNumChoice;
-var userPassLength;
+var lowCase;
+var upCase;
+var specChar;
+var numChoice;
+var passLength;
 
 // Write password to the #password input
 function writePassword() {
@@ -20,11 +20,20 @@ function generatePassword() {
     lengthOfPass = parseInt(lengthOfPass);
 
     var userLowCase = confirm("Should your password have lowercase letters? Click 'OK' for yes or 'Cancel' for no.");
+    var userUpCase = confirm("Should your password have upercase letters? Click 'OK' for yes or 'Cancel' for no.");
+    var userSpecChar = confirm("Should your password have special characters? Click 'OK' for yes or 'Cancel' for no.");
+    var userNumChoice = confirm("Should your password have numbers? Click 'OK' for yes or 'Cancel' for no.");
 
   }
   else {
     alert("The length of your password must be between '8' and '128'.  Click OK to restart.")
   }
+
+
+if (userLowCase === true) {
+lowCase = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","w","x","y","z"];
+for (i = 0; i < lowCase.length; i++);
+}
 
 
 }
@@ -34,3 +43,12 @@ function generatePassword() {
 generateBtn.addEventListener("click", () => {
   writePassword;
 });
+
+
+//-------------temporarily hold my arrays---------------------//
+
+// upcase = ["A","B","C","D","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
+
+// specChar = ["~","'","!","@","#","$","%","^","&","*","(",")","-","_","+","=","{","}","[","]","|","\\","/",":",";","\"","'","<",">",",",".","?"];
+
+// numChoice = [0,1,2,3,4,5,6,7,8,9];
