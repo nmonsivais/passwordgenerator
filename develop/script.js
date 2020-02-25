8ae589e9c5ebbc53cdd9a13115de358f373ee8ae
Tro <noe.monsivais@gmail.com>
2020-02-24 20:37:48 -0600

    Linked generateBTN to EventListener on Click.

--- develop/script.js
+++ develop/script.js
@@ -1,23 +1,29 @@
-// variables //
+// Assignment Code
 var generateBtn = document.querySelector("#generate");
+// variables //
 var userLowCase;
 var userUpCase;
 var userSpecChar;
 var userNumChoice;
 var userPassLength;
 
-// Assignment Code
-var generateBtn = document.querySelector("#generate");
-
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
   passwordText.value = password;
+}
+
+function generatePassword() {
+  var lengthOfPass = prompt("Enter a number between '8' and '128'");
+  if (lengthOfPass >= 8 && lengthOfPass <=128) {
+    lengthOfPass = parseInt(lengthOfPass);
+  }
 
 }
 
+
 // Add event listener to generate button
 generateBtn.addEventListener("click", () => {
- writePassword;
+  writePassword;
 });
