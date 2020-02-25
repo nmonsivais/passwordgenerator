3d85d317fcc33bd0fc0f8f382ec7914ae4703d57
Tro <noe.monsivais@gmail.com>
2020-02-24 20:52:22 -0600

    Added and tested second prompt for lowercase letters.

--- develop/script.js
+++ develop/script.js
@@ -15,10 +15,17 @@ function writePassword() {
 }
 
 function generatePassword() {
-  var lengthOfPass = prompt("Enter a number between '8' and '128'");
+  var lengthOfPass = prompt("Enter a number between '8' and '128' to select the length of your password.");
   if (lengthOfPass >= 8 && lengthOfPass <=128) {
     lengthOfPass = parseInt(lengthOfPass);
+
+    var userLowCase = confirm("Should your password have lowercase letters? Click 'OK' for yes or 'Cancel' for no.");
+
   }
+  else {
+    alert("The length of your password must be between '8' and '128'.  Click OK to restart.")
+  }
+
 
 }
 
